6f7c7957a5683ce30021335df678f70ce3c6ca70
Tyler Yang <0107wy@gmail.com>
2019-07-24 14:45:50 +0900

    Fix fixed fee
    Fee should be configurable through arguments

--- TradingGym/transfer_learning.py
+++ TradingGym/transfer_learning.py
@@ -56,7 +56,7 @@ df.fillna(method='ffill', inplace=True)
 def main():
 
     env = TradingEnv(custom_args=args, env_id='custom_trading_env', obs_data_len=obs_data_len, step_len=step_len, sample_len=sample_len,
-                           df=df, fee=0.001, initial_budget=1, n_action_intervals=n_action_intervals, deal_col_name='c', sell_at_end=True,
+                           df=df, fee=fee, initial_budget=1, n_action_intervals=n_action_intervals, deal_col_name='c', sell_at_end=True,
                            feature_names=['o', 'h','l','c','v',
                                           'num_trades', 'taker_base_vol'])
     agent = dqn_agent.Agent(action_size=2 * n_action_intervals + 1, obs_len=obs_data_len, num_features=env.reset().shape[-1], **hyperparams)
